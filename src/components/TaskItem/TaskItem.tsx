88b1d363c842003aefa2ee3f088dc9835c948e0f
Jimmy Van <jvan709@gmail.com>
2025-06-21 19:27:22 -0600

    style task item

--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import { Task } from '../../types/tasks';
 import { ListItem, Radio } from '@mui/material';
-
+import { StyledTaskItem } from './taskItemStyledComponents';
+import TaskPrioTag from './TaskPrioTag';
+import "./taskItemStyles.css"
 interface TaskItemProps {
     task: Task;
 }
 
 const TaskItem = ({ task }: TaskItemProps) => {
+
+    const currentDate = new Date().toLocaleDateString('en-US', {
+        month: 'numeric',
+        day: '2-digit',
+        year: 'numeric'
+    });
     return (
-        <ListItem className="task-item">
-            <Radio />
-            <span>{task.name}</span>
-            {/* More expandable info can go here */}
-        </ListItem>
+        <StyledTaskItem >
+            <div className='task-item__header'>
+                <span>{currentDate}</span>
+                <div className="task-item__prio-tag">
+                    <TaskPrioTag priority={task.priority} />
+                </div>
+            </div>
+
+            <section className="task-item__main">
+                <div className='task-item__button'>
+                    <Radio />
+                    <span>{task.name}</span>
+                </div>
+                <div className='task-item__tags'>
+                    {task.tags.map((tag) => (
+                        <span key={tag + '-tag'} style={{ marginRight: '0.5rem' }}>#{tag}</span>
+                    ))}
+                </div>
+            </section>
+
+
+        </StyledTaskItem>
+
     );
 };
 
